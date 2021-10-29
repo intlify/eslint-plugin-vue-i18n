8d2657ab2c0115087d9a22ecfe93a6fd3db9e013
Yosuke Ota <otameshiyo23@gmail.com>
2021-10-30 06:03:36 +0900

    Fix false positives for `<i18n-t keypath>` in `no-unused-key` rule (#252)

--- lib/utils/collect-keys.ts
+++ lib/utils/collect-keys.ts
@@ -203,9 +203,10 @@ export function collectKeysFromAST(
         }
       } else {
         if (
-          node.key.name === 'path' &&
-          (node.parent.parent.name === 'i18n' ||
-            node.parent.parent.name === 'i18n-t')
+          (node.key.name === 'path' &&
+            (node.parent.parent.name === 'i18n' ||
+              node.parent.parent.name === 'i18n-t')) ||
+          (node.key.name === 'keypath' && node.parent.parent.name === 'i18n-t')
         ) {
           debug(
             "call VElement:matches([name=i18n], [name=i18n-t]) > VStartTag > VAttribute[key.name='path'] handling ..."
