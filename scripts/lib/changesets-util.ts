bfaf436d05f9ddc6b6181051611978706d7fd568
Yosuke Ota <otameshiyo23@gmail.com>
2023-07-22 10:47:57 +0900

    chore: use changesets (#408)
    * chore: use changesets
    
    * fix

---
+++ scripts/lib/changesets-util.ts
@@ -0,0 +1,10 @@
+import getReleasePlan from '@changesets/get-release-plan'
+import path from 'path'
+
+/** Get new version string from changesets */
+export async function getNewVersion(): Promise<string> {
+  const releasePlan = await getReleasePlan(path.resolve(__dirname, '../..'))
+
+  return releasePlan.releases.find(({ name }) => name === 'eslint-plugin-css')!
+    .newVersion
+}
