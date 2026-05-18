1586a5ef01b1cd3a5774e1b3a5ec795331dc5b37
Andrew Kazakov <andreww_dev@proton.me>
2026-05-18 23:34:57 +0300

    refactor: avoid seenPathDescriptors technique

--- lib/utils/glob-utils.ts
+++ lib/utils/glob-utils.ts
@@ -224,61 +224,53 @@ export function listFilesToProcess(
     })
   })
 
-  const seenPathDescriptors = new Set<string>()
-  const allPathDescriptors = resolvedPathsByGlobPattern.reduce(
-    (pathsForAllGlobs, pathsForCurrentGlob, index) => {
-      if (
-        pathsForCurrentGlob.every(
-          pathDescriptor => pathDescriptor?.behavior === SILENTLY_IGNORE
-        )
-      ) {
-        throw new (
-          pathsForCurrentGlob.length ? AllFilesIgnoredError : NoFilesFoundError
-        )(globPatterns[index])
+  const allPathDescriptors = resolvedPathsByGlobPattern.reduce<
+    Map<string, /* file name */ boolean /* is ignored */>
+  >((pathsForAllGlobs, pathsForCurrentGlob, index) => {
+    if (
+      pathsForCurrentGlob.every(
+        pathDescriptor => pathDescriptor?.behavior === SILENTLY_IGNORE
+      )
+    ) {
+      throw new (
+        pathsForCurrentGlob.length ? AllFilesIgnoredError : NoFilesFoundError
+      )(globPatterns[index])
+    }
+
+    pathsForCurrentGlob.forEach(pathDescriptor => {
+      if (!pathDescriptor || pathsForAllGlobs.has(pathDescriptor.filename)) {
+        return
       }
 
-      pathsForCurrentGlob.forEach(pathDescriptor => {
-        if (
-          !pathDescriptor ||
-          seenPathDescriptors.has(pathDescriptor.filename)
-        ) {
-          return
-        }
+      switch (pathDescriptor.behavior) {
+        case NORMAL_LINT:
+          pathsForAllGlobs.set(pathDescriptor.filename, false)
+          break
+        case IGNORE_AND_WARN:
+          pathsForAllGlobs.set(pathDescriptor.filename, true)
+          break
+        case SILENTLY_IGNORE:
+          // do nothing
+          break
 
-        switch (pathDescriptor.behavior) {
-          case NORMAL_LINT:
-            seenPathDescriptors.add(pathDescriptor.filename)
-            pathsForAllGlobs.push({
-              filename: pathDescriptor.filename,
-              ignored: false
-            })
-            break
-          case IGNORE_AND_WARN:
-            seenPathDescriptors.add(pathDescriptor.filename)
-            pathsForAllGlobs.push({
-              filename: pathDescriptor.filename,
-              ignored: true
-            })
-            break
-          case SILENTLY_IGNORE:
-            // do nothing
-            break
+        default:
+          throw new Error(
+            `Unexpected file behavior for ${pathDescriptor.filename}`
+          )
+      }
+    })
 
-          default:
-            throw new Error(
-              `Unexpected file behavior for ${pathDescriptor.filename}`
-            )
-        }
-      })
+    return pathsForAllGlobs
+  }, new Map())
 
-      return pathsForAllGlobs
-    },
-    [] as {
-      filename: string
-      ignored: boolean
-    }[]
+  const result = Array.from(
+    allPathDescriptors.entries(),
+    ([filename, ignored]) => ({
+      filename,
+      ignored
+    })
   )
 
-  debug(allPathDescriptors)
-  return allPathDescriptors
+  debug(result)
+  return result
 }
